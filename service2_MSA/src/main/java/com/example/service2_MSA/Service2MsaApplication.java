1bd4f781aa52b5da9c89e8052e2b5148a7a20c7b
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-12-08 15:14:59 +0900

    프로젝트 세팅

---
+++ service2_MSA/src/main/java/com/example/service2_MSA/Service2MsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.service2_MSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service2MsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service2MsaApplication.class, args);
+	}
+
+}
---
+++ service2_MSA/src/test/java/com/example/service2_MSA/Service2MsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service2_MSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service2MsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
